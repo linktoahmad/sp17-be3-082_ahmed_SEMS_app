407e5a45e421e433145238d66f26f583cc95901c
Ahmed Khalil <61230494+linktoahmad@users.noreply.github.com>
2021-05-26 21:46:26 +0500

    Add files via upload

---
+++ components/Firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+export default {
+    apiKey: 'AIzaSyAMKuHoGBGGHv7GfQS5wQjh0V_j3mNgm6U',
+    authDomain: 'sems-6fa94.firebaseio.com',
+    databaseURL: 'https://sems-6fa94.firebaseio.com',
+    projectId: 'Xsems-6fa94',
+    storageBucket: 'sems-6fa94.appspot.com',
+    messagingSenderId: '904199881266',
+    appId: '1:904199881266:android:e645438a43a687b421f22b'
+};
+
+    
