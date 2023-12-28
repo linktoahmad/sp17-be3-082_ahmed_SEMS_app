a74f69204400c543daec8f4c2c7a3f22d36b4a9a
Ahmed Khalil <ahmad.khalil@numumail.com>
2023-12-28 20:27:18 +0500

    some serious looking changes

--- components/Firebase/firebaseConfig.js
+++ components/Firebase/firebaseConfig.js
@@ -1,12 +1,9 @@
-
 export default {
-    apiKey: 'AIzaSyAMKuHoGBGGHv7GfQS5wQjh0V_j3mNgm6U',
-    authDomain: 'sems-6fa94.firebaseio.com',
-    databaseURL: 'https://sems-6fa94.firebaseio.com',
-    projectId: 'Xsems-6fa94',
-    storageBucket: 'sems-6fa94.appspot.com',
-    messagingSenderId: '904199881266',
-    appId: '1:904199881266:android:e645438a43a687b421f22b'
+  apiKey: 'AIzaSyApsrej5_flH-nthILdF0KFULYWu1S2mBc',
+  authDomain: 'sems-6fa94.firebaseio.com',
+  databaseURL: 'https://sems-6fa94.firebaseio.com',
+  projectId: 'Xsems-6fa94',
+  storageBucket: 'sems-6fa94.appspot.com',
+  messagingSenderId: '904199881266',
+  appId: '1:904199881266:android:e645438a43a687b421f22b'
 };
-
-    
